6c4ab9d4fb3addb661766258de31818f6be921d8
MANISH KUMAR <selfmanish01@gmail.com>
2023-09-04 15:05:33 +0530

    initial commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAYMPC_v5omND59qRiSyHb6MX9a6mZNF7k",
+    authDomain: "crypter-559dc.firebaseapp.com",
+    projectId: "crypter-559dc",
+    storageBucket: "crypter-559dc.appspot.com",
+    messagingSenderId: "70327679259",
+    appId: "1:70327679259:web:4c68cb139fc7be1436fbbe"
+  };
+
+  export default firebaseConfig;
